d16fc03d9382bc49cb1d29260ffe8a532b16cbab
PierreNedellec <pierrethebear888@gmail.com>
2023-08-13 16:37:02 +0200

    Created give_r function
    Function writes number n as 2^r * q

--- main code.py
+++ main code.py
@@ -3,7 +3,6 @@ Created on Sun Aug 13 13:38:14 2023
 
 Author: Pierre Nedellec
 """
-
 def dec2bin(num):
     b = bin(num)
     b = str(b[2:])
@@ -18,3 +17,11 @@ def modexp(a,b,n): #computes a^b mod n
             (p,j,r) = ((p**2)%n,(j-1)/2,(r*p)%n)
     return r
             
+def give_r(n):#returns r when n-1 written as 2^r * q
+    q = n-1
+    r = 0
+    while q%2 == 0:
+        q /=2
+        r+=1
+    return r
+    
