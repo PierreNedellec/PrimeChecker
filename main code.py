4a8e1f750a70003c641aa928221ecc55e9d1ace0
PierreNedellec <pierrethebear888@gmail.com>
2023-08-13 16:00:33 +0200

    Added the modexp function
    Added the modular exponentiation function to compute (a^b) mod n.

--- main code.py
+++ main code.py
@@ -10,3 +10,12 @@ def dec2bin(num):
     b = str(b[2:])
     return b
 
+def modexp(a,b,n): #computes a^b mod n
+    (p,j,r) = (a,b,1)
+    while j != 0:
+        if j%2 == 0:
+            (p,j,r) = ((p**2)%n,(j/2),(r%n))
+        if j%2 == 1:
+            (p,j,r) = ((p**2)%n,(j-1)/2,(r*p)%n)
+    return r
+            
