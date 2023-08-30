c2db7562e332c0538515d4644e25b585ea33f13c
PierreNedellec <pierrethebear888@gmail.com>
2023-08-30 09:27:25 +0100

    Made small optimisation changes
    Separated line 14 and 16 into three separate lines each for clarity.
    Replaced second j%2 with else.
    Replaced int(str(q)[-1])%2 with q%2 for speed.
    Replaced     r = give_rq(p)[0] and b = give_rq(p)[1] with r,b = give_rq(p) for speed.

--- main code.py
+++ main code.py
@@ -11,22 +11,25 @@ def modexp(a,b,n): #computes a^b mod n
     (p,j,r) = (a,b,1)
     while j != 0:
         if j%2 == 0:
-            (p,j,r) = ((p**2)%n,(divmod(j,2)[0]),(r%n))
-        if j%2 == 1:
-            (p,j,r) = ((p**2)%n,(divmod(j-1,2)[0]),(r*p)%n)
+            p = (p**2)%n
+            j = j//2
+            r = r%n
+        else:
+            p = (p**2)%n
+            j = (j-1)//2
+            r = (r*p)%n
     return r
             
 def give_rq(n):#returns r when n-1 written as 2^r * q
     q = n-1
     r = 0
-    while int(str(q)[-1])%2 == 0:
+    while q%2 == 0:
         q = divmod(q,2)[0]
         r+=1
     return (r,q)
     
 def primality_test(p,a):
-    r = give_rq(p)[0]
-    b = give_rq(p)[1]
+    r,b = give_rq(p)
     if modexp(a,b,p) == 1 or modexp(a,b,p) == p-1:
         return True
     for i in range(r):
